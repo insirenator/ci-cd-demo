1a45096f6d02ea40d22dabf3f9452f28105c96aa
mohdshakeeb <arsshakeeb149@protonmail.com>
2025-02-08 18:44:36 +0530

    Base commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+PORT = process.env.PORT || 3000;
+
+const content = `
+<h1>Hello World</h1>
+<p>I have CI/CD configured</p>
+`
+
+/** 
+ * @param {http.IncomingMessage}
+ * @param {http.ServerResponse}
+ * */
+const handler = (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content);
+}
+
+const server = http.createServer(handler);
+
+server.listen(PORT, () => console.log(`Listening at :${PORT}`));
